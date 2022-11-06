e841cb5e85f1c2c7a789458a201f2754d4816ba1
K1BeR <k1ber@protonmail.com>
2022-11-06 11:30:16 +0300

    v0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='demonlist',
-      version='0.1',
+      version='0.11',
       description='Asynchronous wrapper for the https://demonlist.org/ API',
       packages=find_packages(),
       install_requires=[
@@ -11,6 +11,7 @@ setup(name='demonlist',
             'urllib3',
       ],
       author='Kirill Korolev',
+      url='https://github.com/K1BeR/demonlist-api',
       author_email='k1ber@protonmail.com',
       classifiers=[
             "Programming Language :: Python :: 3.7",
